dc5338b991984a8b8b7af2929abccce85175ee91
Ayush Yadav <ayush.dms.1415@gmail.com>
2026-01-05 17:11:02 +0530

    Initial Commit

---
+++ TeamHub/TeamHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamHubApp.swift
+//  TeamHub
+//
+//  Created by Ayush yadav on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TeamHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
